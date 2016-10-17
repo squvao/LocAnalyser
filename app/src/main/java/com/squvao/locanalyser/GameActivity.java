12d1464f58f820e3a0057c0f068623519b070742
12ase <12ase@kings-school.co.uk>
2016-10-17 21:25:58 +0100

    Changed GameActivity and MainActivity

--- app/src/main/java/com/squvao/locanalyser/GameActivity.java
+++ app/src/main/java/com/squvao/locanalyser/GameActivity.java
@@ -1,5 +1,6 @@
 package com.squvao.locanalyser;
 
+import android.*;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -24,9 +25,9 @@ public class GameActivity extends FragmentActivity implements OnMapReadyCallback
     private LocationListener listener = new android.location.LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
-           /* LatLng point = new LatLng(location.getLatitude(), location.getLongitude());
+            LatLng point = new LatLng(location.getLatitude(), location.getLongitude());
             mMap.addMarker(new MarkerOptions().position(point).title("You're here"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(point));*/
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(point));
             Toast.makeText(GameActivity.this, location.getLatitude() + " " + location.getLongitude(), Toast.LENGTH_SHORT).show();
         }
 
@@ -44,9 +45,9 @@ public class GameActivity extends FragmentActivity implements OnMapReadyCallback
             }
             Location location = manager.getLastKnownLocation(provider);
             Toast.makeText(GameActivity.this, location.getLatitude() + " " + location.getLongitude(), Toast.LENGTH_SHORT).show();
-           /* LatLng point = new LatLng(location.getLatitude(), location.getLongitude());
+            LatLng point = new LatLng(location.getLatitude(), location.getLongitude());
             mMap.addMarker(new MarkerOptions().position(point).title("You're here"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(point));*/
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(point));
         }
 
         @Override
@@ -61,31 +62,12 @@ public class GameActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_game);
         manager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "Resume", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        /*if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        manager.removeUpdates(listener);*/
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        Toast.makeText(this, "OnMapReady", Toast.LENGTH_SHORT).show();
-        mMap = googleMap;
+    private void location(){
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 /*
@@ -93,46 +75,62 @@ public class GameActivity extends FragmentActivity implements OnMapReadyCallback
                 на разрешения доступа к локации. Вывод сообщения если доступ к локации запрещен.
                  */
             Toast.makeText(this, "Access denied", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,android.Manifest.permission.ACCESS_COARSE_LOCATION},123);
             return;
         }
-        //WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
-
-        /*if (manager.getAllProviders().contains(LocationManager.GPS_PROVIDER) && manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+        if (mMap != null) {
+            /*
+            Запрос всем провайдерам на обновление локации с интервалом 0 мили сек.
+             */
             manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
-        }
-        else if (manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) && wifiManager.isWifiEnabled()) {
             manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
-        }
-        else if (Build.VERSION.SDK_INT > Build.VERSION_CODES.ECLAIR_MR1 && manager.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)) {
             manager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, listener);
-        }
-        else {
-            Toast.makeText(this, "Location providers not found", Toast.LENGTH_SHORT).show();
-        }*/
-        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
-        manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
-        manager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, listener);
 
         /*
         PASSIVE_PROVIDER это сигнал сотового оператора
         Build.VERSION.SDK_INT проверка на версию Андроид ECLAIR_MR1 (Андроид 2.1)
          */
-        String[] providers = new String[]{LocationManager.GPS_PROVIDER, LocationManager.NETWORK_PROVIDER, LocationManager.PASSIVE_PROVIDER};
-
-        for (String provider : providers) {
-            Location loc = manager.getLastKnownLocation(provider);
-            if (loc != null){
-                Toast.makeText(this, loc.getLatitude() + " " + loc.getLongitude(), Toast.LENGTH_SHORT).show();
-                LatLng point = new LatLng(loc.getLatitude(), loc.getLongitude());
-                mMap.addMarker(new MarkerOptions().position(point).title("You're here"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(point));
+            String[] providers = new String[]{LocationManager.GPS_PROVIDER, LocationManager.NETWORK_PROVIDER, LocationManager.PASSIVE_PROVIDER};
+            for (String provider : providers) {
+                Location loc = manager.getLastKnownLocation(provider); //получение локации
+                /*
+                Локация может быть не найдена если провайдеры не отвечают
+                Соответственно проверяем ее на пустоту.
+                 */
+                if (loc != null) {
+                    Toast.makeText(this, loc.getLatitude() + " " + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+                    LatLng point = new LatLng(loc.getLatitude(), loc.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(point).title(provider));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(point));
+                }
+                else
+                    Toast.makeText(this, provider + " не сработал", Toast.LENGTH_SHORT).show();
             }
-            else
-                Toast.makeText(this, provider + " не сработал", Toast.LENGTH_SHORT).show();
         }
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Resume", Toast.LENGTH_SHORT).show();
+        location();
+    }
 
+    @Override
+    protected void onPause() {
+        Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show();
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        manager.removeUpdates(listener);
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "OnMapReady", Toast.LENGTH_SHORT).show();
+        mMap = googleMap;
+        location();
         UiSettings uiSettings = mMap.getUiSettings();
         uiSettings.setZoomControlsEnabled(true);
     }
